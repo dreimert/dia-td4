351c184906e03947dd682f0d5ae40fb3c8d6f4cc
Damien Reimert <damien.reimert@gmail.com>
2018-03-01 10:36:06 +0100

    Add script pour read users

---
+++ readDB.js
@@ -0,0 +1,14 @@
+let Db = require('tingodb')().Db;
+let assert = require('assert');
+
+let db = new Db('./db', {});
+
+// Fetch a collection to insert document into
+let collection = db.collection("users");
+
+// Cherche tous les utilisateurs
+collection.find({}).toArray(function(err, result) {
+  if (err) throw err;
+  console.log(result);
+  db.close();
+});
